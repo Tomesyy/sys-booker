f7d7da596d18979783d2010a848e59489e4c759c
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2024-05-21 22:15:20 +0100

    add code editor and execute code button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import CodeEditor from '@uiw/react-textarea-code-editor';
 
 function App() {
+  const [code, setCode] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='code-editor-container'>
+      <CodeEditor 
+        value={code}
+        language='js'
+        placeholder='<Enter code here/>'
+        onChange={(evn) => setCode(evn.target.value)}
+        padding={16}
+        style={{
+          fontSize: '16px',
+          fontWeight: '400px',
+          lineHeight: '20px',
+          backgroundColor: "#f7f7f7",
+          border: '1px solid #000000',
+          borderRadius: '8px',
+          width: '100%',
+          height: '100%',
+          fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+        }}
+      />
+      <button className='code-editor-button'>Run</button>
     </div>
   );
 }
